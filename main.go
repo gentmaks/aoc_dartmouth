0664bbb075b4098fddbb5d4677adf19d512bdacd
Gent Maksutaj <gent.maksutajmax@gmail.com>
2025-12-02 07:51:59 +0100

    day 2 problem 1 sol finshed

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/gentmaks/aoc_dartmouth/day_1/first"
-	"github.com/gentmaks/aoc_dartmouth/day_1/second"
+	"github.com/gentmaks/aoc_dartmouth/day_2/first"
 )
 
 func main() {
 	first.SolveFirst()
-	second.SolveSecond()
 }
